24136464c4a302f663751d64e866245eb7ab9d89
shivamj2603 <shivamj2603@gmail.com>
2020-09-27 14:37:23 +0530

    Wages calculated using switch

--- src/EmployeeWageProblem/EmployeeWageComputation.java
+++ src/EmployeeWageProblem/EmployeeWageComputation.java
@@ -4,20 +4,28 @@ public class EmployeeWageComputation {
 	
 	public static void main(String[] args) {
 		//constants
-		int FULL_TIME = 1;
-		int PART_TIME = 2;
+		final int FULL_TIME = 1;
+		final int PART_TIME = 2;
 		int WAGE_PER_HOUR = 20;
 		//variables
 		int empWage =0;
 		int empHours =0;  
 		int empCheck = (int) Math.floor((Math.random() * 10))%2;
-		if(empCheck == FULL_TIME) {
-			empHours = 8;
-		}
 		
-		else if(empCheck == PART_TIME) {
-			empHours = 4;
-		}
+		switch(empCheck) {
+		
+		case FULL_TIME: 
+			    empHours = 8;
+				break;
+				
+		case PART_TIME: 
+			    empHours = 4;
+				break;
+				
+		default: 
+			     empHours = 0;
+				 break;
+	    }
 		
 		empWage = WAGE_PER_HOUR * empHours;
 		System.out.println("Employee wage is : "+ empWage);

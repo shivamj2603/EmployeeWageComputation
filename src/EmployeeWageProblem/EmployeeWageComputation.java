10cdebcd9175a401b8500924cd2e9c41cf35e515
shivamj2603 <shivamj2603@gmail.com>
2020-09-27 13:48:25 +0530

    Welcome message displayed

---
+++ src/EmployeeWageProblem/EmployeeWageComputation.java
@@ -0,0 +1,9 @@
+package EmployeeWageProblem;
+
+public class EmployeeWageComputation {
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Employee Wage Computation");
+	}
+
+}

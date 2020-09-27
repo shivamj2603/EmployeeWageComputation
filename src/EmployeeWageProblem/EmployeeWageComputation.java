02ed185f11aeacc077e1fa126bb402a653999dd7
shivamj2603 <shivamj2603@gmail.com>
2020-09-27 14:17:31 +0530

    Daily Employee Wage Calculated

--- src/EmployeeWageProblem/EmployeeWageComputation.java
+++ src/EmployeeWageProblem/EmployeeWageComputation.java
@@ -1,19 +1,22 @@
 package EmployeeWageProblem;
 
 public class EmployeeWageComputation {
+	
 	public static void main(String[] args) {
-		    //constants
-			int FULL_TIME = 1;
-			//variables
-			int empCheck = (int) Math.floor((Math.random() * 10))%2;
-			
-			if(empCheck == FULL_TIME) {
-				System.out.println("Employee is Present");
-			}
-			else {
-				System.out.println("Employee is Abscent");
-			}
+		//constants
+		int FULL_TIME = 1;
+		int WAGE_PER_HOUR = 20;
+		//variables
+		int empWage =0;
+		int empHours =0;  
+		int empCheck = (int) Math.floor((Math.random() * 10))%2;
+		if(empCheck == FULL_TIME) {
+			empHours = 8;
+		}
 		
+		empWage = WAGE_PER_HOUR * empHours;
+		System.out.println("Employee wage is : "+ empWage);
+		    
 	}
 
 }

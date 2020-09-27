260915e0d38b1e2880e11b9a9b98a75e15f844e3
shivamj2603 <shivamj2603@gmail.com>
2020-09-27 14:05:24 +0530

    Employee Attendance

--- src/EmployeeWageProblem/EmployeeWageComputation.java
+++ src/EmployeeWageProblem/EmployeeWageComputation.java
@@ -2,8 +2,18 @@ package EmployeeWageProblem;
 
 public class EmployeeWageComputation {
 	public static void main(String[] args) {
+		    //constants
+			int FULL_TIME = 1;
+			//variables
+			int empCheck = (int) Math.floor((Math.random() * 10))%2;
+			
+			if(empCheck == FULL_TIME) {
+				System.out.println("Employee is Present");
+			}
+			else {
+				System.out.println("Employee is Abscent");
+			}
 		
-		System.out.println("Welcome to Employee Wage Computation");
 	}
 
 }

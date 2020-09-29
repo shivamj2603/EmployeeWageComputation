a0ab316e5d27b626fe626acbb40639232a6c6b57
shivamj2603 <shivamj2603@gmail.com>
2020-09-29 20:37:38 +0530

    Refactored:Modified code and implemented ComputeWage method

--- src/EmployeeWageProblem/EmployeeWageComputation.java
+++ src/EmployeeWageProblem/EmployeeWageComputation.java
@@ -1,31 +1,29 @@
 package EmployeeWageProblem;
 
 public class EmployeeWageComputation {
-
-	public static void main(String[] args) {
-		//constants
-		final int FULL_TIME = 1;
-		final int PART_TIME = 2;
-		final int WAGE_PER_HOUR = 20;
-		final int NO_OF_WORKING_DAYS = 20;
-		final int MAX_HOURS_IN_MONTH = 100;
-
-		//variables
-		int empWage = 0;
-		int empHours = 0; 
+	//constants
+	static final int FULL_TIME = 1;
+	static final int PART_TIME = 2;
+	static final int WAGE_PER_HOUR = 20;
+	static final int NO_OF_WORKING_DAYS = 20;
+	static final int MAX_HOURS_IN_MONTH = 100;
+	public static int calculateWage() {
 		int totalEmpWage = 0;
 		int totalEmpHours = 0;
 		int totalWorkingDays = 0;
-
-		while(totalEmpHours < MAX_HOURS_IN_MONTH && totalWorkingDays < NO_OF_WORKING_DAYS) {
-			totalWorkingDays++;
+		while(totalEmpHours < MAX_HOURS_IN_MONTH
+				&& totalWorkingDays < NO_OF_WORKING_DAYS) {
+			int empHours = 0;
+			int empWage = 0;
 			int empCheck = (int) Math.floor((Math.random() * 10)) % 2;
 			switch(empCheck) {
 			case FULL_TIME: 
 				empHours = 8;
+				totalWorkingDays++;
 				break;
 			case PART_TIME: 
 				empHours = 4;
+				totalWorkingDays++;
 				break;
 			default: 
 				empHours = 0;
@@ -33,10 +31,15 @@ public class EmployeeWageComputation {
 			}
 			empWage = WAGE_PER_HOUR * empHours;
 			totalEmpWage += empWage;
-			totalEmpHours += empHours; 
-			System.out.println("Employee Wage : "+ empWage);
+			totalEmpHours += empHours;
+			System.out.println("Daily Employee Wage is :" + empWage);
 		}
-		System.out.println("Total Employee wage is : "+ totalEmpWage);
+		return totalEmpWage;
+	}
+	public static void main(String[] args) {
+		int totalEmpWage;
+		totalEmpWage = calculateWage();
+		System.out.println("Total employee wage : " + totalEmpWage);
 	}
 
 }

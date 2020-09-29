745f35773eb762a800c9fed4b1827414f02100bb
shivamj2603 <shivamj2603@gmail.com>
2020-09-29 22:01:37 +0530

    Added:Interface to compute Employee Wages

---
+++ src/EmployeeWageProblem/CompanyEmployeeWage.java
@@ -0,0 +1,6 @@
+package EmployeeWageProblem;
+
+public interface CompanyEmployeeWage {
+	public void addCompanyEmpWage(String company,int empRate,int noOfWorkingDays,int noOfHoursPerMonth);
+        public void computeEmpWage();
+}

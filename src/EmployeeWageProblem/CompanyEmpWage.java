70ee4d43a1ef64ef112c2daef6f27f5f510eecd4
shivamj2603 <shivamj2603@gmail.com>
2020-09-29 21:51:10 +0530

    Added:Ability to manage Employee Wage of multiple companies

---
+++ src/EmployeeWageProblem/CompanyEmpWage.java
@@ -0,0 +1,25 @@
+package EmployeeWageProblem;
+
+public class CompanyEmpWage {
+	private String company;
+	public int empRate;
+	public int noOfWorkingDays;
+	public int noOfHoursPerMonth;
+	private int totalEmpWage;
+	public CompanyEmpWage(String company,int empRate,int noOfWorkingDays,int noOfHoursPerMonth) {
+		this.company = company;
+		this.empRate = empRate;
+		this.noOfWorkingDays = noOfWorkingDays;
+		this.noOfHoursPerMonth= noOfHoursPerMonth;
+	}
+	public int getTotalEmpWage() {
+		return totalEmpWage;
+	}
+	public void setTotalEmpWage(int totalEmpWage) {
+		this.totalEmpWage = totalEmpWage;
+	}
+	public String toString() {
+		return "Total Employee Wage of " + company + " is " + totalEmpWage;	
+	}
+
+}
--- src/EmployeeWageProblem/EmployeeWageComputation.java
+++ src/EmployeeWageProblem/EmployeeWageComputation.java
@@ -4,22 +4,28 @@ public class EmployeeWageComputation {
 	//constants
 	static final int FULL_TIME = 1;
 	static final int PART_TIME = 2;
-	private String company;
-	private int empRate;
-	private int noOfWorkingDays;
-	private int noOfHoursPerMonth;
-	private int totalEmpWage;
-	public EmployeeWageComputation(String company,int empRate,int noOfWorkingDays,int noOfHoursPerMonth) {
-		this.company = company;
-		this.empRate = empRate;
-		this.noOfWorkingDays = noOfWorkingDays;
-		this.noOfHoursPerMonth= noOfHoursPerMonth;
+	//variables
+	private int numOfCompany = 0;
+	private CompanyEmpWage[] companyEmpWageArray;
+	
+	public EmployeeWageComputation() {
+		companyEmpWageArray = new CompanyEmpWage[5];
 	}
-	int calculateWage() {
+	private void addCompanyEmpWage(String company,int empRate,int noOfWorkingDays,int hoursPerMonth) {
+		companyEmpWageArray[numOfCompany] = new CompanyEmpWage(company, empRate, noOfWorkingDays, hoursPerMonth);
+		numOfCompany++;
+	}
+	private void computeEmpWage() {
+		for(int i = 0; i < numOfCompany; i++) {
+			companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(companyEmpWageArray[i])); 
+			System.out.println(companyEmpWageArray[i]);
+		}
+	}
+	private int computeEmpWage(CompanyEmpWage companyEmpWage) {
 		int totalEmpHours = 0;
 		int totalWorkingDays = 0;
-		while(totalEmpHours < noOfHoursPerMonth
-				&& totalWorkingDays < noOfWorkingDays) {
+		while(totalEmpHours < companyEmpWage.noOfHoursPerMonth
+				&& totalWorkingDays < companyEmpWage.noOfWorkingDays) {
 			int empHours = 0;
 			int empWage = 0;
 			int empCheck = (int) Math.floor((Math.random() * 10)) % 3;
@@ -35,23 +41,17 @@ public class EmployeeWageComputation {
 			default: 
 				empHours = 0;
 				break;
-			}
-			empWage = empRate * empHours;
-			totalEmpWage += empWage;
+			}			
 			totalEmpHours += empHours;
 		}
-		return totalEmpWage;
-	}
-	public String toString() {
-		return "Total Employee Wage of " + company + " is " + totalEmpWage;	
+	    System.out.println("Days : "+totalWorkingDays+" Emp Hours : "+ totalEmpHours);
+		return totalEmpHours * companyEmpWage.empRate;
 	}
 	public static void main(String[] args) {
-		EmployeeWageComputation WalMart = new EmployeeWageComputation("D-Mart",20,30,100);
-		WalMart.calculateWage();
-		System.out.println(WalMart);
-		EmployeeWageComputation Reliance = new EmployeeWageComputation("Reliance",25,25,120);
-		Reliance.calculateWage();
-		System.out.println(Reliance);
-	}
-
+		EmployeeWageComputation empWageBuilder = new EmployeeWageComputation();
+		empWageBuilder.addCompanyEmpWage("WalMart",20,30,100);
+		empWageBuilder.addCompanyEmpWage("Reliance",25,20,120);
+		empWageBuilder.computeEmpWage();
+		}
 }
+		
